c78992b873bb8f4480d11840b4d278de8b00ce98
kaelasyadira <146148661+kaelasyadira@users.noreply.github.com>
2024-10-04 19:51:25 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,61 @@
+const cache_name = 'v1';
+const cache_assets = [
+    'index.html',
+    'contact.html',
+    'about.html',
+    'offline.html',
+    'styles.css',
+];
+
+// Install event
 self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
     event.waitUntil(
-      caches.open('static-assets')
-        .then((cache) => {
-          return cache.addAll([
-            '/',
-            '/index.html',
-            '/style.css',
-            // ...
-          ]);
-        })
+        caches.open(cache_name)
+            .then((cache) => {
+                console.log('Service Worker: Caching Files');
+                return cache.addAll(cache_assets);
+            })
+            .catch((error) => {
+                console.error('Error caching assets:', error);
+            }) 
     );
-  });
-  
-  self.addEventListener('fetch', (event) => {
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+    event.waitUntil(
+        caches.keys().then(
+            (cacheNames) => {
+                return Promise.all(
+                    cacheNames.map(
+                        (cache) => {
+                            if (cache !== cache_name) {
+                                console.log('Service Worker: Clearing Old Cache');
+                                return caches.delete(cache);
+                            }
+                        }
+                    )
+                );
+            }
+        )
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker: Fetching');
     event.respondWith(
-      caches.match(event.request)
-        .then((response) => {
-          return response || fetch(event.request);
-        })
-    );
-  });
+        caches.match(event.request).then(
+            (response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(
+                    () => caches.match('offline.html')
+                );
+            }
+        )
+    );
+});
